d2d37d53f139488a82175b4a498066bce8dfce15
Mario McGee <mario.mcgee@students.makeschool.com>
2020-06-07 16:34:20 -0700

    weather app completed next step is mood add ons

--- weather/views.py
+++ weather/views.py
@@ -1,23 +1,38 @@
 import requests
 from django.shortcuts import render
+from .models import City
+from .forms import CityForm
 
 def index(request):
     url = 'http://api.openweathermap.org/data/2.5/weather?q={}&units=imperial&appid=a4921b98df36266b4b2e362d2041c1bf'
 
-    city = 'Tokyo'
 
-    r = requests.get(url.format(city)).json()
- 
 
-    city_weather = {
-        'city' : city ,
-        'temperature': r['main']['temp'],
-        'description': r['weather'][0]['description'],
-        'icon' : r['weather'][0]['icon'],
+    if request.method == 'POST':
+        form = CityForm(request.POST)
+        form.save()
+    
+    form = CityForm()
 
-    }
+    cities = City.objects.all()
 
-    context = {'city_weather' : city_weather}
+    weather_data= []
+    
+    for city in cities:
+        
+        r = requests.get(url.format(city)).json()
+    
+        city_weather = {
+            'city' : city.name,
+            'temperature': r['main']['temp'],
+            'description': r['weather'][0]['description'],
+            'icon' : r['weather'][0]['icon'],
+
+        }
+
+        weather_data.append(city_weather)
+
+    context = {'weather_data' : weather_data, 'form' : form}
 
 
 
